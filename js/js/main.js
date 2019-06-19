f34ca92f3b6118d72250b63651ec9d83dc7a1a5f
AndriiHrohodza <ahrohodza1@gmail.com>
2019-06-19 21:39:28 +0300

    Completed task first Js

---
+++ js/js/main.js
@@ -0,0 +1,7 @@
+
+var band = ["suicideboys", "ramirez"];// Created my favorite band(1)
+band.push('Oleg Vinnik');// Added Oleg Vinnik to my band(2)
+band.shift();//Delated first element(3)
+band.unshift("rap", "dub");//added 2 favorite types of music(4)
+console.log(band);//(5)
+console.log(band.length);//(6)
