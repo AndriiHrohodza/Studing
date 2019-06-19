45383cc9368bea24fb8c8596e9ac38270e5fc1ff
AndriiHrohodza <ahrohodza1@gmail.com>
2019-06-19 22:04:21 +0300

    Completed Task 2

--- js/js/main.js
+++ js/js/main.js
@@ -1,7 +1,18 @@
-
+/*Task 1*/
 var band = ["suicideboys", "ramirez"];// Created my favorite band(1)
 band.push('Oleg Vinnik');// Added Oleg Vinnik to my band(2)
 band.shift();//Delated first element(3)
 band.unshift("rap", "dub");//added 2 favorite types of music(4)
 console.log(band);//(5)
 console.log(band.length);//(6)
+
+/*Task 2*/
+var arr = ["bmw", "nissan", "audi", "Volvo", "Jeep"];//Create array with 5 elements(1)
+var newArr = arr.slice(2);//Delete 2 elements(2)
+newArr.unshift("Mers");//Added Mers element(3)
+console.log(newArr);//(4)
+newArr[3] = "Toyota";//(5)
+console.log(newArr.length);//(6)
+console.log(newArr);//(7)
+
+
